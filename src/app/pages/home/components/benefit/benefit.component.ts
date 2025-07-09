4d6e0e998e3c04edbefaae00c64347fb50f12285
Ольга <ots231088@gmail.com>
2025-07-09 13:30:06 +0300

    correction components 'benefit'

--- src/app/pages/home/components/benefit/benefit.component.ts
+++ src/app/pages/home/components/benefit/benefit.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
-import {MatDividerModule} from '@angular/material/divider';
+
 
 @Component({
   selector: 'app-benefit',
-  imports: [MatGridListModule,MatDividerModule],
+  imports: [MatGridListModule],
   templateUrl: './benefit.component.html',
   styleUrl: './benefit.component.scss'
 })
